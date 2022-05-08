a879b4ace5eea4b908981beade283df63c8895bc
l3miage-braya <bray.amandine0@gmail.com>
2022-05-08 17:16:19 +0200

    avancement du slide des defis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,7 +39,7 @@ import { CreateEtapeComponent } from './app-views/create/create-etape/create-eta
 import { DragDropModule } from "@angular/cdk/drag-drop";
 import { CreateIndiceComponent } from './app-views/create/create-indice/create-indice.component';
 import { SlidesDefisComponent } from './components/slides-defis/slides-defis.component';
-
+import {IvyCarouselModule} from 'angular-responsive-carousel';
 
 @NgModule({
   declarations: [
@@ -85,6 +85,7 @@ import { SlidesDefisComponent } from './components/slides-defis/slides-defis.com
     AngularFireModule.initializeApp(environment.firebase),
     FormsModule,
     ReactiveFormsModule,
+    IvyCarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
