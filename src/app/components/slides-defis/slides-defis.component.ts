0203e4fb46e20f0fbce48c359131fd66129e9ec8
l3miage-braya <bray.amandine0@gmail.com>
2022-05-10 16:34:32 +0200

    Fin des slides des defis manque juste la syncro avec les images et le synopsis

--- src/app/components/slides-defis/slides-defis.component.ts
+++ src/app/components/slides-defis/slides-defis.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit,NgModule } from '@angular/core';
 import { DefiDto } from 'src/app/api/models';
-
 @Component({
   selector: 'app-slides-defis',
   templateUrl: './slides-defis.component.html',
@@ -8,9 +7,14 @@ import { DefiDto } from 'src/app/api/models';
 })
 export class SlidesDefisComponent implements OnInit {
   @Input() defis!: DefiDto[];
+
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+  trackById(index: number, e:any): number {
+    return e.index;
   }
 
 }
