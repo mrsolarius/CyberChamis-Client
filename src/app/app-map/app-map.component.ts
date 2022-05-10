2941441ef658af117c7e558ecdaab0c47fd38adc
BARKAOUI <meriam.barkaoui@etu.univ-grenoble-alpes.fr>
2022-05-10 12:25:06 +0200

    affichage des marquers

--- src/app/app-map/app-map.component.ts
+++ src/app/app-map/app-map.component.ts
@@ -1,7 +1,6 @@
 import {AfterViewInit, Component} from '@angular/core';
 import * as Leaflet from 'leaflet';
 import {GeolocService} from "../geoLoc/geoloc.service";
-import {latLng, marker} from "leaflet";
 
 @Component({
   selector: 'app-map',
@@ -9,19 +8,25 @@ import {latLng, marker} from "leaflet";
   styleUrls: ['./app-map.component.scss']
 })
 export class AppMapComponent implements AfterViewInit {
+
+
   title = 'AngularOSM';
   private map !: Leaflet.Map;
   private circle!: Leaflet.Circle;
   private localPlace!: Leaflet.Circle;
   // Clusturing
-  markerClusterGroup!: Leaflet.MarkerClusterGroup;
+  //markerClusterGroup!: Leaflet.MarkerClusterGroup;
   markerClusterData = [];
-  markers = Leaflet.markerClusterGroup();
+  markers:Leaflet.MarkerClusterGroup;
 
+  marker!:any;
+  constructor(private loc: GeolocService) {
+    this.markers = Leaflet.markerClusterGroup({removeOutsideVisibleBounds: true});
+  }
 
   ngOnInit () {
-    this.markerClusterGroup = Leaflet.markerClusterGroup({removeOutsideVisibleBounds: true});
   }
+
 
   private async initMap(): Promise<void> {
     const postion = await getPosition()
@@ -31,6 +36,33 @@ export class AppMapComponent implements AfterViewInit {
       center: new Leaflet.LatLng(postion.coords.latitude, postion.coords.longitude),
       minZoom: 2
     });
+    let greenIcon = Leaflet.icon({
+      iconUrl: 'assets/placeholder.png',
+
+      iconSize:     [30, 55], // size of the icon
+      //iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+    let markers = Leaflet.markerClusterGroup({ chunkedLoading: true});
+    markers.addLayer(Leaflet.marker([45.188559,5.794524],{ title: 'p1' }));
+    const mList=[]
+    mList.push(Leaflet.marker([45.188528,5.724525], {icon :greenIcon}).bindPopup('defi1'));
+    mList.push(Leaflet.marker([45.188521,5.724526],{title: 'p3'}).bindPopup('p3'));
+    mList.push(Leaflet.marker([45.188522,5.724527],{title: 'p4'}).bindPopup('p4'));
+    mList.push(Leaflet.marker([45.188523,5.724526],{title: 'p5'}).bindPopup('p5'));
+    mList.push(Leaflet.marker([45.188524,5.784525],{title: 'p6'}).bindPopup('p6'));
+    mList.push(Leaflet.marker([45.188888,5.744525],{title: 'p7'}).bindPopup('p7'));
+    mList.push(Leaflet.marker([45.188526,5.764526],{title: 'p8'}).bindPopup('p8'));
+    mList.push(Leaflet.marker([45.188527,5.744524],{title: 'p9'}).bindPopup('p9'));
+    mList.push(Leaflet.marker([45.188510,5.784524],{title: 'p10'}).bindPopup('p10'));
+    mList.push(Leaflet.marker([45.188529,5.734524],{title: 'p11'}).bindPopup('p11'));
+    mList.push(Leaflet.marker([45.188539,5.724524],{title: 'p12'}).bindPopup('p12'));
+    mList.push(Leaflet.marker([45.188549,5.774524],{title: 'p13'}).bindPopup('p132'));
+
+    markers.addLayers(mList);
+    //console.log(markers.getChildCount());
+    this.map.addLayer(markers);
 
     console.log(postion)
     this.circle = Leaflet.circle([postion.coords.latitude, postion.coords.longitude], {
@@ -49,8 +81,8 @@ export class AppMapComponent implements AfterViewInit {
 
 
     const tiles = Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
+      maxZoom: 5,
+      minZoom: 5,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
@@ -98,9 +130,6 @@ export class AppMapComponent implements AfterViewInit {
       this.localPlace.setRadius(10)
     else
       this.localPlace.setRadius(2.5)
-  }
-  constructor(private loc: GeolocService) {
-
   }
 
   ngAfterViewInit(): void {
